994ae0c3a8523a351e5959f455e1e9b5b362975b
Lucas <lucasreiste@gmail.com>
2022-01-03 19:25:51 -0300

    Criando Global Styles e primeira prototipos da primeira tela

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
---
+++ src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title isRed={false}>
+        Login
+        <small>Oie</small>
+      </Title>
+      <Paragrafo>Lorem Ipsum</Paragrafo>
+      <button type="button">Login</button>
+    </Container>
+  );
+}
